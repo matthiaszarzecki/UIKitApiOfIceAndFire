7b7b6e3fc11bdeb798aec2eb3082cb1e115f5948
matthiaszarzecki <matthias.zarzecki@gmail.com>
2024-03-11 12:06:32 +0100

    Add project

---
+++ UIKitApiOfIceAndFire/UIKitApiOfIceAndFire/UIKitApiOfIceAndFireApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitApiOfIceAndFireApp.swift
+//  UIKitApiOfIceAndFire
+//
+//  Created by Matthias Zarzecki on 11.03.24.
+//
+
+import SwiftUI
+
+@main
+struct UIKitApiOfIceAndFireApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
